c57cb510d8fec9696e50523aa2d99899579ea3ce
hayarov <edurda77@gmail.com>
2024-04-01 17:44:37 +0500

    v1_1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.kapt)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -30,17 +32,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.7"
     }
     packaging {
         resources {
@@ -66,4 +68,16 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Dagger
+    implementation (libs.hilt.android)
+    annotationProcessor (libs.google.hilt.compiler)
+    kapt (libs.google.hilt.compiler)
+    implementation (libs.androidx.hilt.navigation.compose)
+
+    implementation (libs.accompanist.systemuicontroller)
+}
+
+kapt{
+    correctErrorTypes = true
 }
---
+++ app/src/main/java/com/propokeignintion/cardrules/AndreiApp.kt
@@ -0,0 +1,7 @@
+package com.propokeignintion.cardrules
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AndreiApp: Application ()
