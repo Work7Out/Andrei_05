f798c90917bb901cd07b084648900fa9855aaf68
hayarov <edurda77@gmail.com>
2024-04-02 18:00:40 +0500

    v1_13

--- app/src/main/java/com/propokeignintion/cardrules/AndreiApp.kt
+++ app/src/main/java/com/propokeignintion/cardrules/AndreiApp.kt
@@ -1,7 +1,16 @@
 package com.propokeignintion.cardrules
 
 import android.app.Application
+import com.onesignal.OneSignal
+import com.onesignal.debug.LogLevel
 import dagger.hilt.android.HiltAndroidApp
 
+const val ONE_SIGNAL = "e6ff5fa2-0e52-48d5-89e0-1da5ce069001"
 @HiltAndroidApp
-class AndreiApp: Application ()
+class AndreiApp: Application () {
+    override fun onCreate() {
+        super.onCreate()
+        OneSignal.Debug.logLevel = LogLevel.VERBOSE
+        OneSignal.initWithContext(this, ONE_SIGNAL)
+    }
+}
